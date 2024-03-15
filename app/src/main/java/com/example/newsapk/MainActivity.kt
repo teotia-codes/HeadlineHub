2b054da6142cfb6c1a71745d31c2d6df60ad98e2
teotia-codes <priyanshu.teotia50@gmail.com>
2024-03-15 20:02:16 +0530

    NavGraph and Removing Systembar colors

--- app/src/main/java/com/example/newsapk/MainActivity.kt
+++ app/src/main/java/com/example/newsapk/MainActivity.kt
@@ -1,54 +1,65 @@
 package com.example.newsapk
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
 import com.example.newsapk.domain.usecases.AppEntryUseCases
+import com.example.newsapk.presentation.navgraph.NavGraph
 import com.example.newsapk.presentation.onboarding.OnBoardingScreen
 import com.example.newsapk.presentation.onboarding.OnBoardingViewModel
 import com.example.newsapk.ui.theme.NewsApkTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var useCases: AppEntryUseCases
+   val viewModel by viewModels<MainViewModel>()
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
-        lifecycleScope.launch {
-            useCases.readAppEntry().collect{
-                Log.d("Test",it.toString())
-            }
+        installSplashScreen().apply {
+            viewModel.splashCondition
         }
+
         WindowCompat.setDecorFitsSystemWindows(window,false)
         setContent {
             NewsApkTheme {
                 // A surface container using the 'background' color from the theme
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemController = rememberSystemUiController()
+                SideEffect {
+                    systemController.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)){
-                        val viewModel: OnBoardingViewModel = hiltViewModel()
-                        OnBoardingScreen(
-                            event = viewModel::onEvent
-                        )
+                      val startDestination = viewModel.startDestination
+                        NavGraph(startDestination = startDestination)
                     }
 
                 }
---
+++ app/src/main/java/com/example/newsapk/MainViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.newsapk
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import com.example.newsapk.domain.usecases.AppEntryUseCases
+import com.example.newsapk.presentation.navgraph.Route
+import javax.inject.Inject
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+@HiltViewModel
+class MainViewModel  @Inject
+    constructor(
+        private val appEntryUseCases: AppEntryUseCases
+    )
+    : ViewModel() {
+        var splashCondition by mutableStateOf(true)
+            private set
+
+    var startDestination by mutableStateOf(Route.AppStartNavigation.route)
+        private set
+init {
+    appEntryUseCases.readAppEntry().onEach { shouldStartFromHomeScreen->
+        if (shouldStartFromHomeScreen){
+            startDestination = Route.NewsNavigation.route
+        }else {
+            startDestination = Route.AppStartNavigation.route
+        }
+        delay(300)
+        splashCondition = false
+    }.launchIn(viewModelScope)
+}
+
+}
